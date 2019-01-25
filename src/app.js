1e87c5460ca03b25be5b0b50b41a4961678051fd
Shubham <arya.xoert@gmail.com>
2019-01-26 02:21:38 +0530

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
+// import { addExpense } from "./actions/expenses";
 // import { editExpense } from "./actions/expenses";
 // import { removeExpense } from "./actions/expenses";
 // import { setTextFilter } from "./actions/filters";
@@ -11,7 +11,7 @@ import { addExpense } from "./actions/expenses";
 // import { sortByAmount } from "./actions/filters";
 // import { setStartDate } from "./actions/filters";
 // import { setEndDate } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
+// import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -19,38 +19,38 @@ const store = configureStore();
 
 // store.subscribe(() => {});
 
-store.dispatch(
-  addExpense({
-    description: "water bill",
-    note: "semester end",
-    amount: 3000,
-    createdAt: 1548352979923
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     description: "water bill",
+//     note: "semester end",
+//     amount: 3000,
+//     createdAt: 1548352979923
+//   })
+// );
 
-store.dispatch(
-  addExpense({
-    description: "gas bill",
-    note: "semester end",
-    amount: 5000,
-    createdAt: 1548352979923
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     description: "gas bill",
+//     note: "semester end",
+//     amount: 5000,
+//     createdAt: 1548352979923
+//   })
+// );
 
-store.dispatch(
-  addExpense({
-    description: "Electricity bill",
-    note: "semester end",
-    amount: 1500,
-    createdAt: 1548352979923
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     description: "Electricity bill",
+//     note: "semester end",
+//     amount: 1500,
+//     createdAt: 1548352979923
+//   })
+// );
 
 // store.dispatch(setTextFilter("bill"));
 
-const state_v = store.getState();
-const visibleExpenses = getVisibleExpenses(state_v.expenses, state_v.filters);
-console.log(visibleExpenses);
+// const state_v = store.getState();
+// const visibleExpenses = getVisibleExpenses(state_v.expenses, state_v.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
